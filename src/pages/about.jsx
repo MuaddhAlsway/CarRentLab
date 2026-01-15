d6f4ad25c983a84a4721c91eee31aa6036575387
Muaddh AL-Sway <150309909+MuaddhAlsway@users.noreply.github.com>
2026-01-15 15:17:06 +0300

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Aboutus from '../component/Aboutus'
+import Aboutus from '../component/aboutus'
 import TrustProffesional from '../component/TrushProfessionals'
 import Email from '../component/Email'
 import Footer from '../component/Footer'
